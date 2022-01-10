6abc432d3b13878be8274d75c4281d6618ad1330
CryDeTaan <CryDeTaan@users.noreply.github.com>
2022-01-10 16:42:24 +0200

    Obtain Access token using Identity.Client

--- SecureWeatherClient/Program.cs
+++ SecureWeatherClient/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Identity.Client;
+
+Console.WriteLine("Making the call...");
+RunAsync().GetAwaiter().GetResult();
+
+static async Task RunAsync()
+{
+    AuthConfig config = AuthConfig.ReadFromJsonFile("appsettings.json");
+
+    IConfidentialClientApplication app;
+
+    app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+        .WithClientSecret(config.ClientSecret)
+        .WithAuthority(new Uri(config.Authority))
+        .Build();
+
+    string[] ResourceIds = new string[] {config.ResourceID};
+
+    AuthenticationResult result = null;
+    try
+    {
+        result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Token acquired \n");
+        Console.WriteLine(result.AccessToken);
+        Console.ResetColor();
+    }
+    catch (MsalClientException ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(ex.Message);
+        Console.ResetColor();
+    }
+}
